46bb9765b7ac6e1fc2e1e3c5e2969804e20c42b0
Jonathan Pillajo <pandc19@outlook.com>
2024-07-07 07:22:42 -0500

    fin sección 15

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { AuthModule } from './auth/auth.module';
+import { MessageWsModule } from './message-ws/message-ws.module';
 
 @Module({
   imports: [
@@ -37,6 +38,8 @@ import { AuthModule } from './auth/auth.module';
     FilesModule,
 
     AuthModule,
+
+    MessageWsModule,
   ],
 })
 export class AppModule { }
---
+++ src/message-ws/message-ws.gateway.ts
@@ -0,0 +1,67 @@
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { JwtService } from '@nestjs/jwt';
+
+import { Server, Socket } from 'socket.io';
+
+import { MessageWsService } from './message-ws.service';
+import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtPayload } from '../auth/interfaces';
+
+@WebSocketGateway({ cors: true })
+export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  @WebSocketServer() wss: Server;
+
+  constructor(
+    private readonly messageWsService: MessageWsService,
+    private readonly jwtService: JwtService,
+  ) { }
+
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.authentication as string;
+    let payload: JwtPayload;
+
+    try {
+      payload = this.jwtService.verify(token);
+      await this.messageWsService.registerClient(client, payload.id);
+
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+
+    // console.log({ payload });
+
+    // console.log('Client connected: ', client.id);
+
+    this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
+
+  }
+  handleDisconnect(client: Socket) {
+    // console.log('Client disconnected: ', client.id);
+    this.messageWsService.removeClient(client.id);
+
+    this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
+  }
+
+  @SubscribeMessage('message-from-client')
+  onMessageFromClient(client: Socket, payload: NewMessageDto) {
+
+    //! Emite únicamente al cliente.
+    // client.emit('message-from-server', {
+    //   fullName: 'Soy Yo!',
+    //   message: payload.message || 'no-message',
+    // });
+
+    //! Emitir a todos MENOS, al cliente inicial
+    // client.broadcast.emit('message-from-server', {
+    //   fullName: 'Soy Yo!',
+    //   message: payload.message || 'no-message',
+    // });
+
+    this.wss.emit('message-from-server', {
+      fullName: this.messageWsService.getUserFullName(client.id),
+      message: payload.message || 'no-message',
+    });
+  }
+}
